861351175ec7e5f7f8013026d274dcba7c07b085
NetAcad DEVASC <changeme@example.com>
2025-06-30 00:44:58 +0000

    Configuration via SSH view and functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import socket, uuid, platform, psutil
+import netmiko
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
@@ -11,6 +12,33 @@ def get_diveces_info():
             "ram": psutil.virtual_memory().percent,
             "mac_address":''.join(['{:02x}'.format((uuid.getnode()>>ele)&0xef) for ele in range(0,48,8)])
             }
+
+def send_device_command(ip):
+    try:
+        device = {
+            'device_type': 'cisco_ios',
+            'ip': ip,
+            'username': 'cisco',
+            'password': 'Cisco123! ',
+            'secret': 'class',
+            'port': 22,
+            'verbose': True
+        }
+        connection = netmiko.ConnectHandler(**device)
+        connection.enable()
+        commands = ['hostname Router1']
+        #commands = 'hostname GNS3-Router'
+        output = connection.send_config_set(commands) #para una lista de comandos
+        #output = connection.send_command(commands)  # para un solo comando
+        connection.disconnect()
+        if '% Invalid input' in output or '% Incomplete command' in output:
+            status = "Error de configuración"
+            return status
+        else:
+            status = "Comando ejecutado con éxito"
+            return status
+    except Exception as e:
+        return str(e)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -34,7 +62,13 @@ def devices():
 @app.route('/devices/configure', methods=['GET','POST'])
 def devices_configure():
     if request.method == 'GET':
-        return render_template('devices_configure.html')
+        ip = request.args.get('ip')
+        if ip:
+            output = send_device_command(ip)
+            print(f'Output from device {ip}: {output}')
+            return render_template('devices_configure.html', ip=ip, output=output)
+        else:
+            return render_template('devices_configure.html', error="No IP address provided")
 
 
 @app.route('/device-info', methods=['GET'])
